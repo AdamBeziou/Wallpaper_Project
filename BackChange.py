0e3247f224b25cca50496333bba58b6e7eeada32
AdamBeziou <31672539+AdamBeziou@users.noreply.github.com>
2017-11-30 12:01:42 -0700

    Delete BackChange.py

--- BackChange.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-
-def BackChange(src):
-	SPI_SETDESKWALLPAPER = 0x14
-	SPIF_UPDATEINIFILE = 0x2
-	ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, src, SPIF_UPDATEINIFILE)
-	
-if __name__ == '__main__':
-    BackChange(src)
